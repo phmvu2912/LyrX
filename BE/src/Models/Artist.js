7c1636af22a32380347c13f07ef9268402e0c215
phmvu2912 <phmvu2912@gmail.com>
2025-02-13 22:51:16 +0700

    Create model Artist

---
+++ BE/src/Models/Artist.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const Artist = new Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    background: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+    },
+    views_per_month: {
+        type: Number,
+        default: 0
+    },
+    is_verified: {
+        type: Boolean,
+        default: false
+    }
+});
+
+export default mongoose.model('Artist', Artist);
